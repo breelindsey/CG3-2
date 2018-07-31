805200e38a3c86a556bea2020752f317907121e9
breelindsey <35509667+breelindsey@users.noreply.github.com>
2018-07-30 22:24:37 -0500

    Required comment.

--- CG3-2/Program.cs
+++ CG3-2/Program.cs
@@ -6,7 +6,24 @@ namespace CG3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Ask the user to guess a number between 1 and 10.
+            If they enter 8, output You Win!.  Otherwise output You Lose. 
+            Do this using an inline if statement. 
+            Upload your program to a new repository called CG 3-2.*/
+
+            
+            Console.Write("Guess a number between 1-10: ");
+            
+            //This string assigns the variable 'guess' to the user input.
+           string guess = Console.ReadLine();
+           
+            //This string tells the program how to respond to the user input.
+            string message = (guess == "8") ? "You Win!" : "You Lose!";
+           
+            //This WriteLine prints the result of the user input.
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
+        
     }
 }
